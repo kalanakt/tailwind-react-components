52d8db7add05d7f2fa54d00b5d74efff46898223
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-12 17:21:40 +0530

    1

---
+++ public/icons/navbar/IconAdminD.js
@@ -0,0 +1,51 @@
+export default function IconAdminD() {
+    return (
+      <svg
+        width="40"
+        height="40"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 300 300"
+        style={{
+          enableBackground: "new 0 0 300 300",
+        }}
+        xmlSpace="preserve"
+      >
+        <path
+          style={{
+            fill: "#005ece",
+          }}
+          d="M40 220h40v80H40zM40 110h40v80H40zM40 0h40v80H40z"
+        />
+        <path
+          style={{
+            fill: "#2488ff",
+          }}
+          d="M0 220h40v80H0zM0 110h40v80H0zM0 0h40v80H0z"
+        />
+        <path
+          style={{
+            fill: "#005ece",
+          }}
+          d="M260 220h40v80h-40zM260 110h40v80h-40zM260 0h40v80h-40z"
+        />
+        <path
+          style={{
+            fill: "#2488ff",
+          }}
+          d="M220 220h40v80h-40zM220 110h40v80h-40zM220 0h40v80h-40z"
+        />
+        <path
+          style={{
+            fill: "#005ece",
+          }}
+          d="M150 220h40v80h-40zM150 110h40v80h-40zM150 0h40v80h-40z"
+        />
+        <path
+          style={{
+            fill: "#2488ff",
+          }}
+          d="M110 220h40v80h-40zM110 110h40v80h-40zM110 0h40v80h-40z"
+        />
+      </svg>
+    )
+  }
---
+++ public/icons/navbar/IconVue.js
@@ -0,0 +1,23 @@
+export default function IconVue(){
+    return (
+      <svg 
+        width="40" 
+        height="40" 
+        viewBox="0 0 32 32" 
+        xmlns="http://www.w3.org/2000/svg"
+        >
+          <path 
+            d="M24.4,3.925H30L16,28.075,2,3.925H12.71L16,9.525l3.22-5.6Z" 
+            fill="#41b883"
+          />
+          <path 
+            d="M2,3.925l14,24.15L30,3.925H24.4L16,18.415,7.53,3.925Z" 
+            fill="#41b883"
+          />
+          <path 
+            d="M7.53,3.925,16,18.485l8.4-14.56H19.22L16,9.525l-3.29-5.6Z" 
+            fill="#35495e"
+          />
+      </svg>
+    )
+  }
