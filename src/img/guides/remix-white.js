7e223236d1d13ebdaf6d0adc315712a51922766c
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-21 06:43:01 +0530

    fix svg import error

--- src/img/guides/remix-white.js
+++ src/img/guides/remix-white.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export default function RailsLogoWhite() {
+export default function RemixLogoWhite() {
   return (
     <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
